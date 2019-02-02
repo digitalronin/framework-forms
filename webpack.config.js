c90d891bf4e785e234b06723bb23ce380d392b7f
David Salgado <david@digitalronin.com>
2019-02-02 12:33:15 +0000

    Formatting tweak: put css rule on a single line

--- webpack.config.js
+++ webpack.config.js
@@ -14,10 +14,7 @@ module.exports = {
   })],
   module: {
     rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
     ],
   },
 };
