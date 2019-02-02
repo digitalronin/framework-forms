478b9331a759a0d8258c80096619bac1a2816223
David Salgado <david@digitalronin.com>
2019-02-02 14:34:36 +0000

    Add pretty-printing the html output
    This is lifted from StackOverflow:
    
    https://stackoverflow.com/questions/26360414/javascript-how-to-correct-indentation-in-html-string#26361620

--- src/classes/Transformer.js
+++ src/classes/Transformer.js
@@ -21,8 +21,40 @@ class Transformer {
     let label = $h.children()[0];
     $(label).attr('for', id);
 
-    return $h.html();
+    $h.children().wrapAll('<div class="form-group">');
+
+    return this.process($h.html());
   }
+
+  // https://stackoverflow.com/questions/26360414/javascript-how-to-correct-indentation-in-html-string#26361620
+  process(str) {
+    var div = document.createElement('div');
+    div.innerHTML = str.trim();
+    return this.format(div, 0).innerHTML;
+  }
+
+  format(node, level) {
+    var indentBefore = new Array(level++ + 1).join('  '),
+      indentAfter  = new Array(level - 1).join('  '),
+      textNode;
+
+    for (var i = 0; i < node.children.length; i++) {
+
+      textNode = document.createTextNode('\n' + indentBefore);
+      node.insertBefore(textNode, node.children[i]);
+
+      this.format(node.children[i], level);
+
+      if (node.lastElementChild == node.children[i]) {
+        textNode = document.createTextNode('\n' + indentAfter);
+        node.appendChild(textNode);
+      }
+    }
+
+    return node;
+  }
+
+
 }
 
 export { Transformer };
