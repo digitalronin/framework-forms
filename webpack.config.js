0767e87e86c3b1721ed2cc74f1617da83840ef70
David Salgado <david@digitalronin.com>
2019-02-02 12:33:30 +0000

    Move mailing list signup to a partial

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
   })],
   module: {
     rules: [
+      { test: /\.html$/, use: ['html-loader?interpolate'] },
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
     ],
   },
