5e95dfaa50c265137ce1b4fa4881a1e7cef8811d
David Salgado <david@digitalronin.com>
2019-02-03 12:49:56 +0000

    Show initial html example, on load

--- src/app.js
+++ src/app.js
@@ -3,10 +3,24 @@ import { Transformer } from './classes';
 
 const transformer = new Transformer();
 
+const exampleHtml = `
+<label>Email</label>
+<input id="email" type="email" placeholder="foo@bar.com" />
+
+<label>Password</label>
+<input id="password" type="password" placeholder="Password" />
+`;
+
+const transform = () => {
+  const input = $('#plainFormTextarea').val();
+  const transformed = transformer.transform(input);
+  $('#transformedFormTextarea').val(transformed);
+};
+
 $().ready(() => {
-  $('#plainFormTextarea').on('keyup', () => {
-    const input = $('#plainFormTextarea').val();
-    const transformed = transformer.transform(input);
-    $('#transformedFormTextarea').val(transformed);
-  });
+  $('#plainFormTextarea').on('keyup', transform);
+
+  // display initial example
+  $('#plainFormTextarea').val(exampleHtml);
+  transform();
 });
